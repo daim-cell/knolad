ad7ede6eb2338ed7c97fd66996e8b403fb634403
moodercoder <2017n1191@gmail.com>
2024-06-12 20:03:41 +0500

    bilal

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,15 +1,30 @@
-
-import  openDB  from '../../../utils/database';
+import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'jsonwebtoken';
+import  openDB  from '../../../utils/database'
 import {  NextResponse } from 'next/server';
-export async function POST(req: NextResponse) {
+
+const JWT_SECRET = "konald"
+
+export async function POST(req: Request) {
+  console.log("here")
+  if (req.method === 'POST') {
     const body = await req.json()
     const { username, password } = body;
     const db = await openDB();
+    console.log(db)
     const user = await db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password]);
+    console.log(user)
     if (user) {
-      return NextResponse.json({ message: 'Login successful' });
+      // res.status(200).json({ message: 'Login successful' });
+      const token = jwt.sign({ userId: user.id }, JWT_SECRET , {
+        expiresIn: '1m',
+      })
+      console.log(token)
+      return NextResponse.json({ token })
     } else {
-      return NextResponse.json({ message: 'Invalid username or password' });
+      NextResponse.json({ status: 400 ,message: 'Invalid username or password' });
     }
-  
+  } else {
+    NextResponse.json({ message: 'Method not allowed' });
+  }
 }
